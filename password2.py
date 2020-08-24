23afb2419eedb53fe06a1a4d4c46bd4924335310
Kingston Kei <kingstonkei1998@gmail.com>
2020-08-24 11:43:45 +1000

    remove 0 version

--- password2.py
+++ password2.py
@@ -1,13 +1,10 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i = i -1
 	pw = input('enter your password: ')
 	if pw == password:
 		print('login successfully')
 		break
 	else:
-		i = i - 1
 		print('password incorrect! you still have' , i, 'chances')
-		if i == 0:
-			print('login failed')
-			break
